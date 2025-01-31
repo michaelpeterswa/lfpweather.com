91d5855f403363f0ff72079d0967dfc258f05ca0
Michael Peters Jr <michael@michaelpeterswa.com>
2025-01-31 15:27:04 -0800

    fix: add anchor link tag guys

--- components/layout/container/title.tsx
+++ components/layout/container/title.tsx
@@ -1,7 +1,23 @@
+import { Link as LinkIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function Title({ title }: { title: string }) {
+  const linkName = stripNonAlphanumeric(title.replace(" ", "-")).toLowerCase();
   return (
     <>
-      <h1 className="text-4xl text-center w-full mb-4">{title}</h1>
+      <div
+        className="flex justify-center items-baseline gap-2 w-full mb-4"
+        id={linkName}
+      >
+        <h1 className="text-4xl">{title}</h1>
+        <Link href={`#${linkName}`} scroll={false}>
+          <LinkIcon className="h-3 w-3 text-gray-600" />
+        </Link>
+      </div>
     </>
   );
+}
+
+function stripNonAlphanumeric(input: string): string {
+  return input.replace(/[^a-zA-Z0-9-]/g, "");
 }
